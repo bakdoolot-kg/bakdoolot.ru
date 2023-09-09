b179936c3ab6bab1a1e53f78c6b6947c66ad7c52
Bakdoolot A <altybaevbaku15@gmail.com>
2023-09-09 11:06:39 +0600

    Update ScrollUp.jsx

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -5,8 +5,8 @@ const ScrollUp = () => {
   window.addEventListener("scroll", function () {
     const scrollUp = document.querySelector(".scrollup");
 
-    if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-    else scrollUp.classList.remove("show-scroll");
+    if (this.scrollY >= 560) scrollUp.classList.add(".show-scroll");
+    else scrollUp.classList.remove(".show-scroll");
   });
 
   return (
